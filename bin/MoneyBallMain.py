d20ca44961de82ea6434a979a9424554b93bc7d1
jamesdi1993 <jamesyubai.di@gmail.com>
2016-10-25 04:27:45 -0700

    Add rscript call in application Main

--- bin/MoneyBallMain.py
+++ bin/MoneyBallMain.py
@@ -16,7 +16,8 @@ def main() :
 	subprocess.call("python resultUpdator.py -s 2016-2017", shell = True)
 	# pull new stats from basketball-reference.com
 	subprocess.call("python statsScraper.py -s 2016-2017", shell = True)
-
+    # join the stats tables and schedule table for today
+    subprocess.call("RScript mergeTables_JD.R -s 2016-2017")
 	# train model and make prediction
 	training_data_path = "../data/2014_2015/2014_2015_full_table.csv"
 	prediction_data_path = "../data/2016_2017/schedule/" + today + "/" + today + "-full-table.csv"
@@ -25,10 +26,10 @@ def main() :
 	logRegPredictor = logRegression.LogRegressionNaivePredictor()
 	logRegPredictor.train_w_file(training_data_path)
 	y_pred = logRegPredictor.test_w_file(prediction_data_path)
-
 	print "The prediction for: ", today, " is: ", y_pred
+	# write the prediction to the schedule file
 	logRegPredictor.writeToFile(schedule_path, y_pred)
-	
+
 
 if __name__ == "__main__" :
    main()
