9a2942d347a9fc1658ab93f1a6ce359b4bebdb27
jamesdi1993 <jamesyubai.di@gmail.com>
2017-03-21 09:47:42 -0700

    Add tool to generate standings table from the season result.
    1) Since the historical team standings were not stored for the season
    previous to 2016-2017, this change adds a tool to reproduce the
    team standings for a given day provided the season game results.
    2) Generated the team standings for the 2015-2016 season.
    3) Clean up some past data that is no longer used.

--- data/2015_2016/tmp/2015-12-01_files/container_data_002/a.js
+++
@@ -1,19 +0,0 @@
-
-var ftGUID_2172185="32187A2BE7216A";
-var ftConfID_2172185="0";
-var ftParams_2172185="ftx=&fty=&ftadz=&ftscw=&ft_custom=&ftOBA=1&ft_ifb=1&ft_domain=www.basketball-reference.com&ft_agentEnv=0&ft_referrer=http%3A%2F%2Fwww.basketball-reference.com%2Fboxscores%2Findex.cgi%3Fmonth%3D12%26day%3D01%26year%3D2015&cachebuster=247400.87424821244";
-var ftKeyword_2172185="";
-var ftCustom_2172185="";
-var ftSection_2172185="";
-var ftSegment_2172185="";
-var ftSegmentList_2172185=[];
-var ftRuleMatch_2172185="0";
-var ftTimestamp_2172185="1477207373";
-var ftImp2172185 = {};
-ftImp2172185.privacy = "x";
-ftImp2172185.guidAge = "-1";
-ftImp2172185.impID = "3218586DA95655";
-
-
-document.write('<scr'+'ipt src="https://cdn.flashtalking.com/xre/217/2172185/1583823/js/j-2172185-1583823.js" id="ftscript_m2172185" name="ftscript_m2172185"></scr'+'ipt>');
-                    
